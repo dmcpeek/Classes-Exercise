b28f8aaf5b1f0c5409e3996724911171b278b54f
Doug McPeek <dmcpeek@outlook.com>
2023-04-19 20:54:35 -0600

    First check in

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+            myCar.Make = "Toyota";
+            myCar.Model = "RAV4";
+            myCar.Year = 2017;
+            Console.WriteLine($"My car is a {myCar.Year} {myCar.Make} {myCar.Model}");
         }
     }
 }
